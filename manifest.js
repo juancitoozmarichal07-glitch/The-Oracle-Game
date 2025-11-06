8d76b84736a893506a504595ca6712e94b1c94a8
juancitoozmarichal07-glitch <juancitoozmarichal.07@gmail.com>
2025-11-06 11:00:36 -0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+    "name": "The Oracle Game",
+    "short_name": "Oracle Game",
+    "description": "Adivina el personaje secreto que el Oráculo ha concebido.",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#000000",
+    "theme_color": "#ff00ff",
+    "icons": [
+        {
+            "src": "/icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any maskable"
+        },
+        {
+            "src": "/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+        }
+    ]
+}
